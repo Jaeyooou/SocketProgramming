a571d1febd160461e7b4168d1f0901bdba26590a
jaeyooou <jaehyen2@naver.com>
2021-05-07 17:57:31 +0900

    Add create socket function and bind

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 This is ComputerNetwork Assignment2
 OS: ubuntu
 Socket Programming with C language
+
+
+--- vim setting - ----
+
+
+" 자동 문법 강조
+syntax on
+
+"color테마를 적용
+":colorscheme spectro   "테마를 설치할 경우에 적용가능하다.
+
+set nocompatible "Vi 와의 호환성을 없애고, Vim 만의 기능을 쓸 수 있게 함.
+set hi=1000 "명령어 기록을 남길 갯수 지정
+set bs=indent,eol,start "백스페이스 사용
+
+"encoding setting
+set enc=utf-8
+set fenc=utf-8
+set fencs=utf-8,cp949,cp932,euc-jp,shift-jis,big5,ucs-2le,latin1
+
+
+set cindent "C언어 자동 들여쓰기 옵션
+set autoindent " 자동 들여쓰기 옵션
+set smartindent 
+
+set tabstop=2    "탭에 대한 공백 크기 설정
+set shiftwidth=2   "autoindent 옵션이 존재할때 후향 탭의 공백의 수를 정의
+set nu      "라인 번호
+set hls     "검색어 강조
+
+"프로그램 시작시 플러그인 로드
+set lpl
+
+" 괄호 자동 완성 후 입력모드로 전환
+map! () ()i
+map! (); ();hi
+map! [] []i
+map! {} {}i
+map! {}; {};iO
+map! <> <>i
+map! '' ''i
+map! "" ""i
+
+"vim plug-in on
+filetype plugin on
+
+
+
+출처: https://www.morenice.kr/5 [morenice's blog]
---
+++ server.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "netinet/in.h"
+#include <arpa/inet.h>
+
+int main(int argc , char *argv[]){
+// argc : number of parameters , argv : value of paramater , argv[0] : route
+// if gcc -o server.c 1234 -> argc : 2 , argv[1] = '1234'          
+	int serv_socket;
+    int client_socket;
+    struct sockaddr_in serv_address;
+	char message[25];
+	int str_len;
+    
+    printf("number of parameter : %d\n" , argc);
+    printf("value is %s\n" , argv[1]);
+    
+	if(argc != 3){
+		printf("parameter is not allowed\n");
+		return 0;
+	}
+    if(strcmp(argv[2] , "TCP") == 0){
+		printf("this protocol is TCP \n");
+		serv_socket = socket(AF_INET , SOCK_STREAM , 0);
+		if(serv_socket == -1){
+			printf("Socket Error\n");
+			return 0;
+		}
+		else{
+			printf("init Socket Done\n");
+		}
+	}
+	else if(strcmp(argv[2] , "UDP") == 0){
+		printf("this protocol is UDP\n");
+		serv_socket = socket(AF_INET , SOCK_DGRAM , 0);
+		if(serv_socket == -1){
+			printf("Socket Error\n");
+			return 0;}
+		else
+			printf("init Socket Done\n");
+		
+	}
+	memset(&serv_address ,0 , sizeof(serv_address));
+	serv_address.sin_family = AF_INET; // IPv4
+	serv_address.sin_addr.s_addr = htonl(INADDR_ANY); // htonl,htons : convert little endian to big endian , INADDR_ANY :my ipAdress
+	serv_address.sin_port = htons(atoi(argv[1]));
+	
+	if(bind(serv_socket , (struct sockaddr*)&serv_address , sizeof(serv_address)) == -1){
+	printf("Binding error\n");
+	return 0;
+	}
+
+	
+		
+    //socket(int domain , int type , int protocol)
+    //int domain-> AF_UNIX : between same process
+    // AF_INET : IPv4 , AP_INET6 : IPv6
+    // type -> SOCK_STREAM(TCP) , SOCK_DGRAM(UDP) , SOCK_RAW(RAW)
+    // int protocol -> IPPROTO_TCP : TCP , IPPROTO_UDP : UDP
+    // socket return -1 if socket error , and success -> value that is >=0 socket descriptor
+        
+
+
+
+
+
+    
+    
+	
+}
