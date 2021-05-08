093a5f5626547d0ef1cdcd1aa7b5b32cd1fe7d4b
jaeyooouinthebuildin <jaehyen2@naver.com>
2021-05-09 02:10:35 +0900

    Add Chat Function with TCP

--- client.c
+++ client.c
@@ -10,45 +10,73 @@
 #define BUFSIZE 1024
 
 int main(int argc , char *argv[]){
-	int clnt_socket;
-	char sendMessage[BUFSIZE];
-	char receiveMessage[BUFSIZE];
-	size_t send_len; // unsigned data type
-	char name[BUFSIZE];
-	struct sockaddr_in serv_address , clnt_address;
-	
-	int clnt_address_size;
-	// argv[0] route , argv[1] protocol type , argv[2] server_ip_address ,  argv[3] port_num , argv[4] client_name
-	if( argc != 5){
-		printf("Parameter Error \n");
-		return 0;
-	}
-	strcpy(name , argv[4]);
-	printf("Client name : %s\n",name);
-	if(strcmp(argv[1] , "TCP") == 0 || strcmp(argv[1] , "tcp")== 0){ //TCP
-		printf("This Connection protocol is TCP\n");
-		clnt_socket = socket(AF_INET , SOCK_STREAM , 0);
-	}
-	else if(strcmp(argv[1],"UDP") == 0 || strcmp(argv[1],"udp")==0){ // UDP
-		printf("This Connection protocol is UDP\n");
-		clnt_socket = socket(AF_INET,SOCK_DGRAM,0);
-	}
-	if(clnt_socket == -1){
-		printf("Socket Error\n");
-		return 0;
-	}
-	memset(&serv_address , 0 , sizeof(serv_address));
-	serv_address.sin_family = AF_INET;
-	serv_address.sin_addr.s_addr = inet_addr(argv[2]);
-	serv_address.sin_port = htons(atoi(argv[3]));
-	// init server_address struct
-	
-	if(connect(clnt_socket , (struct sockaddr*)&serv_address , sizeof(serv_address)) == -1){
-	printf("Connection error\n");
-	return 0;
-	}
+    int clnt_socket;
+    char send_message[BUFSIZE];
+    char receive_message[BUFSIZE];
+    size_t send_len; // unsigned data type
+    char name[BUFSIZE];
+    struct sockaddr_in serv_address , clnt_address;
+    int clnt_address_size;
+    // argv[0] route , argv[1] protocol type , argv[2] server_ip_address ,  argv[3] port_num , argv[4] client_name
+    if( argc != 5){
+        printf("Parameter Error \n");
+        return 0;
+    }
+    strcpy(name , argv[4]);
+    printf("Client name : %s\n",name);
+    if(strcmp(argv[1] , "TCP") == 0 || strcmp(argv[1] , "tcp")== 0){ //TCP
+        printf("This Connection protocol is TCP\n");
+        clnt_socket = socket(AF_INET , SOCK_STREAM , 0);
+        if(clnt_socket == -1){
+            printf("Socket Error\n");
+            return 0;
+        }
+        memset(&serv_address , 0 , sizeof(serv_address));
+        serv_address.sin_family = AF_INET;
+        serv_address.sin_addr.s_addr = inet_addr(argv[2]);
+        serv_address.sin_port = htons(atoi(argv[3]));
+        // init server_address struct
+        
+        if(connect(clnt_socket , (struct sockaddr*)&serv_address , sizeof(serv_address)) == -1){
+        printf("Connection error\n");
+        return 0;
+        }
 
-	printf("chatting start , if you want to exit input 'q' or 'quit' \n");
-
-	
+        printf("chatting start , if you want to exit input 'q' or 'quit' \n");
+        while(1){
+            //init message
+            printf("[%s]:",name);
+            memset(send_message , 0 , BUFSIZE);
+            fgets(send_message , BUFSIZE , stdin);
+            if(strcmp(send_message , "quit") == 0 || strcmp(send_message , "q") == 0){
+                printf("chatting is over\n");
+                return 0;
+            }
+            send_len = strlen(send_message);
+            ssize_t send_num = write(clnt_socket , send_message , BUFSIZE);
+            if(send_num == -1){
+                printf("Write Error!\n");
+                return 0;
+            }
+            if(strcmp(receive_message , "quit") == 0 || strcmp(receive_message , "q") == 0){
+                printf("chatting is over\n");
+                return 0;
+            }
+            printf("[server]%s: %s\n",inet_ntoa(serv_address.sin_addr) , receive_message);
+            memset(receive_message , 0 , sizeof(receive_message));
+        
+        }
+    }
+    else if(strcmp(argv[1],"UDP") == 0 || strcmp(argv[1],"udp")==0){ // UDP
+        printf("This Connection protocol is UDP\n");
+        clnt_socket = socket(AF_INET,SOCK_DGRAM,0);
+        if(clnt_socket == -1){
+            printf("Socket Error\n");
+            return 0;
+        }
+    }
+   
+    
 }
+
+
--- server.c
+++ server.c
@@ -8,82 +8,112 @@
 #include "netinet/in.h"
 #include <arpa/inet.h>
 
+#define BUFSIZE 1024
 int main(int argc , char *argv[]){
 // argc : number of parameters , argv : value of paramater , argv[0] : route
-// if gcc -o server.c 1234 -> argc : 2 , argv[1] = '1234'          
-	//argv[0] route  , argv[1] protocol type , argv[2] server_port_num
-	int serv_socket;
+// if gcc -o server.c 1234 -> argc : 2 , argv[1] = '1234'
+    //argv[0] route  , argv[1] protocol type , argv[2] server_port_num
+    int serv_socket;
     int client_socket;
-	struct sockaddr_in serv_address;
-	struct sockaddr_in clnt_address;
-	char message[25];
-	int str_len;
-	int clnt_address_size;
+    struct sockaddr_in serv_address;
+    struct sockaddr_in clnt_address;
+    char receive_message[BUFSIZE] , send_message[BUFSIZE]; // chatting message re
+    int str_len;
+    int clnt_address_size;
    // printf("my address : %d",)
     
-	if(argc != 3){
-		printf("parameter is not allowed\n");
-		return 0;
-	}
+    if(argc != 3){
+        printf("parameter is not allowed\n");
+        return 0;
+    }
     if(strcmp(argv[1] , "TCP") == 0 || strcmp(argv[1] , "tcp") == 0){
-		printf("this protocol is TCP \n");
-		serv_socket = socket(AF_INET , SOCK_STREAM , 0);
-	
-	}
-	else if(strcmp(argv[1] , "UDP") == 0 || strcmp(argv[1] , "udp") == 0){
-		printf("this protocol is UDP\n");
-		serv_socket = socket(AF_INET , SOCK_DGRAM , 0);}
-		
-	 if(serv_socket == -1){
-		 printf("Socket Error\n");
-		 return 0;
-	 }
+        printf("this protocol is TCP \n");
+        serv_socket = socket(AF_INET , SOCK_STREAM , 0);
+        //socket(int domain , int type , int protocol)
+        //int domain-> AF_UNIX : between same process
+        // AF_INET : IPv4 , AP_INET6 : IPv6
+        // type -> SOCK_STREAM(TCP) , SOCK_DGRAM(UDP) , SOCK_RAW(RAW)
+        // int protocol -> IPPROTO_TCP : TCP , IPPROTO_UDP : UDP
+        // socket return -1 if socket error , and success -> value that is >=0 socket descriptor
+        if(serv_socket == -1){
+            printf("Socket Error\n");
+            return 0;
+        }
 
-	memset(&serv_address ,0 , sizeof(serv_address));
-	serv_address.sin_family = AF_INET; // IPv4
-	serv_address.sin_addr.s_addr = htonl(INADDR_ANY); // htonl,htons : convert little endian to big endian , INADDR_ANY :my ipAdress
-	serv_address.sin_port = htons(atoi(argv[2]));
-	
-	if(bind(serv_socket , (struct sockaddr*)&serv_address , sizeof(serv_address)) == -1){
-	printf("Binding error\n");
-	return 0;
-	}
-	printf("create TCP socket(%d :%d) done\n" ,htonl( INADDR_ANY) , atoi(argv[2]));
-	if(listen(serv_socket ,5) == -1)
-	{
-		printf("Listening error\n");
-		return 0;
-	}
-	printf("Server lIstening~~~~\n");
-	//server listening
-	clnt_address_size = sizeof(clnt_address);
-	client_socket = accept(serv_socket , (struct sockaddr*)&clnt_address , &clnt_address_size);
-	//if accept is successed  ,func accept() returns clients socket, 
-	if(client_socket == -1){
-		printf("accept error\n");
-		return 0;
-	}
-	printf("Enter Client\n");
-	printf("Connected from %s:%d\n" ,inet_ntoa(clnt_address.sin_addr),ntohs(clnt_address.sin_port));
-	
-	
-
-
-	
-		
-    //socket(int domain , int type , int protocol)
-    //int domain-> AF_UNIX : between same process
-    // AF_INET : IPv4 , AP_INET6 : IPv6
-    // type -> SOCK_STREAM(TCP) , SOCK_DGRAM(UDP) , SOCK_RAW(RAW)
-    // int protocol -> IPPROTO_TCP : TCP , IPPROTO_UDP : UDP
-    // socket return -1 if socket error , and success -> value that is >=0 socket descriptor
+        memset(&serv_address ,0 , sizeof(serv_address));
+        serv_address.sin_family = AF_INET; // IPv4
+        serv_address.sin_addr.s_addr = htonl(INADDR_ANY); // htonl,htons : convert little endian to big endian , INADDR_ANY :my ipAdress
+        serv_address.sin_port = htons(atoi(argv[2]));
         
-
-
-
-
-
+        if(bind(serv_socket , (struct sockaddr*)&serv_address , sizeof(serv_address)) == -1){
+        printf("Binding error\n");
+        return 0;
+        }
+        printf("create TCP socket(%d :%d) done\n" ,htonl( INADDR_ANY) , atoi(argv[2]));
+        if(listen(serv_socket ,5) == -1)
+        {
+            printf("Listening error\n");
+            return 0;
+        }
+        printf("Server lIstening~~~~\n");
+        //server listening
+        clnt_address_size = sizeof(clnt_address);
+        client_socket = accept(serv_socket , (struct sockaddr*)&clnt_address , &clnt_address_size);
+        //if accept is successed  ,func accept() returns clients socket,
+        if(client_socket == -1){
+            printf("accept error\n");
+            return 0;
+        }
+        printf("Enter Client\n");
+        printf("Connected from %s:%d\n" ,inet_ntoa(clnt_address.sin_addr),ntohs(clnt_address.sin_port));
+        while(1){
+            //read() from client
+            memset(receive_message , 0 , sizeof(receive_message));
+            ssize_t Received_num = read(client_socket , receive_message , BUFSIZE);
+            if(Received_num == -1){
+                printf("receive Error\n");
+                return 0;
+            }
+            printf("from %s : %s\n",inet_ntoa(clnt_address.sin_addr) , receive_message);
+            
+            if(strcmp(receive_message , "q") == 0 || strcmp(receive_message , "quit") == 0){
+                printf("Chatting quit!\n");
+                return 0;
+            }
+            // init receive_message buffer , send_message buffer
+            memset(receive_message , 0 , sizeof(receive_message));
+            memset(send_message , 0 , sizeof(send_message));
+            // input server message
+            fgets(send_message , BUFSIZE , stdin);
+            if(strcmp(send_message , "q") == 0 || strcmp(send_message , "quit") == 0){
+                printf("Chatting quit!\n");
+                return 0;
+            }
+            ssize_t send_num = write(client_socket , send_message , BUFSIZE);
+            if(send_num == -1){
+                printf("Send Error! \n");
+                return 0;
+            }
+            
+            
+            close(client_socket);
+            close(serv_socket);
+        }
+    
+               }
     
     
-	
+//--------------------TCP Protocol--------------------------------------------
+    else if(strcmp(argv[1] , "UDP") == 0 || strcmp(argv[1] , "udp") == 0){
+        printf("this protocol is UDP\n");
+        serv_socket = socket(AF_INET , SOCK_DGRAM , 0);
+        if(serv_socket == -1){
+            printf("Socket Error\n");
+            return 0;
+        }
+
+    }
+    
+    
 }
+
